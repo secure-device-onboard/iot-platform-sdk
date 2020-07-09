b395832b5e5bb4c57620949f618ad8d3fbc51ac3
Davis Benny <davis.benny@intel.com>
2020-07-09 08:53:58 +0530

    Fix proxy configuration in Docker Build Script.
       1. Fixes the typoerror in proxy configuration.
    
       This commit also focuses on
    
       2. Fixing whitesapce/checkstyle issues,by removing all trailing
          whitespaces and changing the indentation of conditional statement.
    
       3. Fixing apt to apt-get ,as we are getting warnings for using the apt.
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- build/build.sh
+++ build/build.sh
@@ -1,22 +1,28 @@
+#!/bin/bash
+
+# Copyright 2020 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+
 export http_proxy_host=$(echo $http_proxy | awk -F':' {'print $2'} | tr -d '/')
 export http_proxy_port=$(echo $http_proxy | awk -F':' {'print $3'} | tr -d '/')
 
 export https_proxy_host=$(echo $https_proxy | awk -F':' {'print $2'} | tr -d '/')
 export https_proxy_port=$(echo $https_proxy | awk -F':' {'print $3'} | tr -d '/')
 
-export _JAVA_OPTIONS="-Dhttp.proxyHost=$http_proxy_host -Dhttp.proxyPort=$http_proxy_port -Dhttps.proxyHost=$https_proxy_host -Dhttps.proxyPort=$http_proxy_port"
+export _JAVA_OPTIONS="-Dhttp.proxyHost=$http_proxy_host -Dhttp.proxyPort=$http_proxy_port -Dhttps.proxyHost=$https_proxy_host -Dhttps.proxyPort=$https_proxy_port"
 
 REMOTE_URL=https://github.com/secure-device-onboard/iot-platform-sdk
 REMOTE_BRANCH=master
 
-if [ "$use_remote" = "1" ]; 
-    then 
-        cd /tmp/
-        git clone $REMOTE_URL
-        cd /tmp/iot-platform-sdk/ 
-        git checkout $REMOTE_BRANCH
-        mvn clean install 
-        cd demo && find . -name \*.war -exec cp --parents {} /home/sdouser/iot-platform-sdk/demo \;      
-    else 
-        mvn clean install 
-fi 
+if [ "$use_remote" = "1" ]; then
+  echo "Building $REMOTE_URL : $REMOTE_BRANCH"
+  cd /tmp/
+  git clone $REMOTE_URL
+  cd /tmp/iot-platform-sdk/
+  git checkout $REMOTE_BRANCH
+  mvn clean install
+  cd demo && find . -name \*.war -exec cp --parents {} /home/sdouser/iot-platform-sdk/demo \;
+else
+  echo "Building local copy"
+  mvn clean install
+fi
